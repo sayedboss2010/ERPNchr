46aa0712f2c7b0ed5fc22a58f72f6f7fcfabfcbe
Administrator <Administrator@DESKTOP-NOQBEUP>
2025-12-01 18:25:03 +0200

    sayed

---
+++ ERPEmeco/Areas/LookUP/Controllers/JopDataController.cs
@@ -0,0 +1,143 @@
+﻿using EF.Data;
+using EF.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VM.ViewModels;
+
+
+namespace ERPNchr.Areas.LookUP.Controllers
+{
+    [Area("LookUP")]
+    public class JopDataController : Controller
+    {
+        private readonly AppDbContext _context = new AppDbContext();
+
+        public IActionResult Index()
+        {
+            var data = (from l in _context.HrJobs
+                        //where l.IsActive == true
+                        orderby l.Id descending
+                        select new JopDataVM
+                        {
+                            Id = l.Id,
+                            TitleAr = l.TitleAr,
+                            TitleEn = l.TitleEn,
+                            IsActive = l.IsActive,
+                        }).ToList();
+
+            return View(data);
+        }
+
+
+        [HttpGet]
+
+        public ActionResult CreateEdite(int TypePage, int? JobID)
+        {
+            JopDataVM model = new JopDataVM();
+
+            // For Edit (TypePage = 2) or View (TypePage = 3)
+            if ((TypePage == 2 || TypePage == 3) && JobID.HasValue)
+            {
+                model = _context.HrJobs
+                    .Where(b => b.Id == JobID.Value)
+                    .Select(b => new JopDataVM
+                    {
+                        Id = b.Id,
+                        TitleAr = b.TitleAr,
+                        TitleEn = b.TitleEn,
+                        IsActive = b.IsActive,
+                        
+                    })
+                    .FirstOrDefault();
+
+                if (model == null)
+                    return NotFound();
+            }
+
+            return View(model);
+        }
+
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateEdite(HrJob HRJobs)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(HRJobs);
+            }
+
+            if (HRJobs.Id == 0)   // CREATE
+            {
+                int newId = _context.Database
+                    .SqlQueryRaw<int>("SELECT NEXT VALUE FOR dbo.HR_HRJob_SEQ")
+                    .AsEnumerable()
+                    .First();
+
+                var entity = new HrJob
+                {
+                    Id = newId,
+                    TitleAr = HRJobs.TitleAr,
+                    TitleEn = HRJobs.TitleEn,
+                    CreatedDate = DateOnly.FromDateTime(DateTime.Now),
+                    CreatedUserId = 1,
+                    IsActive = false
+                };
+
+                _context.HrJobs.Add(entity);
+            }
+            else                      // UPDATE
+            {
+                var entityold = _context.HrJobs.Find(HRJobs.Id);
+
+                if (entityold == null)
+                {
+                    return NotFound(); ;
+                }
+
+                entityold.TitleAr = HRJobs.TitleAr;
+                entityold.TitleEn = HRJobs.TitleEn;
+
+                entityold.UpdatedDate = DateOnly.FromDateTime(DateTime.Now);
+                entityold.UpdatedUserId = 1;
+
+                _context.HrJobs.Update(entityold);
+            }
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index", "HRJob", new { area = "LookUP" });
+        }
+
+
+       
+
+     
+        [HttpPost]
+        public async Task<IActionResult> ActivateBranch(int id, bool isActive)
+        {
+            var entity = await _context.HrJobs.FindAsync(id);
+
+            if (entity == null)
+                return Json(new { success = false, message = "Not found" });
+
+            // عكس الحالة (Toggle)
+            entity.IsActive = !isActive;
+
+            entity.UpdatedDate = DateOnly.FromDateTime(DateTime.Now);
+            entity.UpdatedUserId = 1;
+
+            await _context.SaveChangesAsync();
+
+            return Json(new
+            {
+                success = true,
+                redirectUrl = Url.Action("Index", "HRJob", new { area = "LookUP" })
+            });
+        }
+
+
+    }
+}
---
+++ VM/ViewModels/LookUp/JopDataVM.cs
@@ -0,0 +1,41 @@
+﻿using EF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VM.ViewModels
+{
+    public class JopDataVM
+    {
+        public int Id { get; set; }
+
+        public string? TitleAr { get; set; }
+
+        public string? TitleEn { get; set; }
+
+        public decimal? MinSalary { get; set; }
+
+        public decimal? MaxSalary { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public int? CreatedUserId { get; set; }
+
+        public DateOnly? CreatedDate { get; set; }
+
+        public int? UpdatedUserId { get; set; }
+
+        public DateOnly? UpdatedDate { get; set; }
+
+        public int? DeletedUserId { get; set; }
+
+        public DateOnly? DeletedDate { get; set; }
+
+
+        public virtual ICollection<HrBranchDepartment> HrBranchDepartments { get; set; } = new List<HrBranchDepartment>();
+
+        public virtual ICollection<HrMachineIp> HrMachineIps { get; set; } = new List<HrMachineIp>();
+    }
+}
